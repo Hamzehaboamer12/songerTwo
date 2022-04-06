bce37a320d81882cf02b117fb01a79a3beddb80c
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-04-06 05:30:20 +0300

    add

--- songer-two/src/main/java/com/example/songertwo/Model/Albums.java
+++ songer-two/src/main/java/com/example/songertwo/Model/Albums.java
@@ -1,63 +1,42 @@
 package com.example.songertwo.Model;
 
 
-import javax.annotation.Generated;
+import javax.persistence.Id;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+@Entity
 public class Albums {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-
-    private int id;
     private int length;
     private int songCount;
     private String title;
     private String imageUrl;
     private String artist;
 
+
+
     public Albums() {
-
     }
 
-
-    public Albums( int id , int length, int songCount, String title, String imageUrl, String artist) {
-        this.id = id;
-        this.title = title;
-        this.artist = artist;
-        this.songCount = songCount;
+    public Albums(int length, int songCount, String title, String imageUrl, String artist) {
         this.length = length;
+        this.songCount = songCount;
+        this.title = title;
         this.imageUrl = imageUrl;
-
+        this.artist = artist;
     }
 
-
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setSongCount(int songCount) {
-        this.songCount = songCount;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
     }
 
     public int getLength() {
@@ -78,5 +57,25 @@ public class Albums {
 
     public String getArtist() {
         return artist;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
     }
 }
--- songer-two/src/test/java/com/example/songertwo/SongerTwoApplicationTests.java
+++ songer-two/src/test/java/com/example/songertwo/SongerTwoApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SongerTwoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
